ddce388c95c7fce2ce0aae5fa551e5cd382e9ce9
Siddhesh <bhosalesiddhesh2000@gmail.com>
2024-01-09 22:55:51 +0530

    layout app

--- React_04-Starting With Code/App.js
+++ React_04-Starting With Code/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { ShoppingBag } from "lucide-react";
+import { ShoppingBag, Search, Home, PercentCircle, Info } from "lucide-react";
 
 import resList from "./mockData";
+
 
 //Header Component
 
@@ -19,11 +20,29 @@ const Header = () => {
 
       <nav className="nav">
         <ul className="nav-links">
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li className="profile">
-            <ShoppingBag />
+          <li>
+            <a href="/">
+              <Home />
+              <span>Home</span>
+            </a>
+          </li>
+          <li>
+            <a href="/about">
+              <PercentCircle />
+              <span>Offers</span>
+            </a>
+          </li>
+          <li>
+            <a href="/contact">
+              <Info />
+              <span>Help</span>
+            </a>
+          </li>
+          <li>
+            <a href="/shopping">
+              <ShoppingBag />
+              <span>Cart</span>
+            </a>
           </li>
         </ul>
       </nav>
@@ -31,61 +50,92 @@ const Header = () => {
   );
 };
 
-const CardContainer = ({foodData}) => {
+const CardContainer = ({ foodData }) => {
+  if (!foodData || !foodData.info) {
+    return null; // or a placeholder for loading or error state
+  }
+
+  const {
+    id,
+    cloudinaryImageId,
+    name,
+    avgRating,
+    cuisines,
+    costForTwo,
+    sla,
+  } = foodData?.info; //Optional Chaining
+  const {deliveryTime} = sla;
+
   // above we will use destructing for props
+  
   return (
     <>
-      {foodData.map((foodInfo) => {
-        const imageKey = foodInfo.info.cloudinaryImageId;
-        return (
-          <div className="card" key={foodInfo.info.id}>
-            <img
-              className="food-img"
-                src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/"+imageKey}
-            />
+      <div className="card">
+        <img
+          className="food-img"
+          src={
+            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/" +
+            cloudinaryImageId
+          }
+        />
 
-            <div className="food-heading">
-              <h4>{foodInfo.info.name}</h4>
-              <div className="rating">{foodInfo.info.avgRating} &#x2606;</div>
-            </div>
+        <div className="food-heading">
+          <h4>{name}</h4>
+          <div className="rating">{avgRating} &#x2606;</div>
+        </div>
 
-            <div className="food-dis">
-              <p>{foodInfo.info.cuisines.join(', ')}</p>
-              <p>{foodInfo.info.costForTwo}</p>
-            </div>
+        <div className="food-dis">
+          <p>{cuisines.join(", ")}</p>
+          <p>{costForTwo}</p>
+        </div>
 
-            <div className="food-time">
-              <p>{foodInfo.info.sla.deliveryTime} min</p>
-            </div>
-          </div>
-        );
-      })}
+        <div className="food-time">
+          <p>{deliveryTime} min</p>
+        </div>
+      </div>
     </>
   );
 };
 
-// const Search = ()=> {
-
-// }
+const SearchBar = () => {
+  return (
+    <div className="search-container">
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search for restaurants and food "
+      />
+      <button className="search-button">
+        <Search />
+      </button>
+    </div>
+  );
+};
 
 const FoodCards = () => {
+
+  //!Note: never take index's as a key react says it.
+  const cardComponents = resList.map((restro) => (
+    <CardContainer key={restro.info.id}  foodData={restro}/>
+  ));
+
   return (
     <main>
-      <div>Search</div>
+      <SearchBar />
       <div id="card-container">
-        <CardContainer foodData={resList} />
+        {cardComponents}
       </div>
     </main>
   );
 };
+
 const AppParent = () => {
   return (
     <div id="app">
-      {/* //Header */}
       <Header />
-      {/* //Body */}
       <FoodCards />
-      {/* //Footer */}
+
+      {/* Footer */}
     </div>
   );
 };
